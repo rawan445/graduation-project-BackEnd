f31c7d489491308fd77d5e29f7e036f5e5952d5f
Rawan Zaben <91823735+RawanZaben@users.noreply.github.com>
2021-12-15 21:53:24 +0300

    Delete Real_Estate_BackEnd directory

--- Real_Estate_BackEnd/routers/routes/signUpRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const signUpRoute = express.Router();
-
-const { addUser } = require("../controllers/signUp");
-
-signUpRoute.post("/signUp", addUser);
-
-module.exports = signUpRoute;
